c10d661e6629f27190b7e7f49f7051f902cccde9
Kirill12a <kirilldrozd12@gmail.com>
2021-04-19 21:49:04 +0300

    Initial Commit

---
+++ AlamafireTableViewData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamafireTableViewData
+//
+//  Created by Kirill Drozdov on 19.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
